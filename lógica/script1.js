2b4e89582f66f2e46bc5f0c7213cd42488db77c9
Paola Soares Machado <paolasesi351@gmail.com>
2026-03-12 08:35:43 -0300

    Adicionar arquivos iniciais de JavaScript com exemplos de comparação de idade

---
+++ lógica/script1.js
@@ -0,0 +1,8 @@
+let idadeMinima = 18;
+let idadeUsuario = "18";
+
+console.log(idadeUsuario > 10);
+console.log(idadeUsuario >= 18);
+
+console.log(idadeUsuario == 18);
+console.log(idadeUsuario === 18);   
